e48a84a01737932e1200f88193078d6b950de2a5
alimbetov <51784151+alimbetov@users.noreply.github.com>
2026-08-22 23:58:10 +0500

    feat(m1): expose domain persistence contracts

---
+++ src/astra_indexator/domain/__init__.py
@@ -0,0 +1,8 @@
+from .contracts import AccessZoneCode, CANONICAL_ACCESS_ZONES, DocumentIdentity, KnowledgeType
+
+__all__ = [
+    "AccessZoneCode",
+    "CANONICAL_ACCESS_ZONES",
+    "DocumentIdentity",
+    "KnowledgeType",
+]
