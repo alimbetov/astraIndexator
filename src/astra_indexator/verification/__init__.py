83b8078db719a80d2dbdadb298dc33576df31624
alimbetov <51784151+alimbetov@users.noreply.github.com>
2026-08-23 23:56:50 +0500

    feat: add verification package for TZ-17 linguistic corpus

---
+++ src/astra_indexator/verification/__init__.py
@@ -0,0 +1,15 @@
+from .linguistic import (
+    BoundaryMetrics,
+    CorpusReport,
+    evaluate_corpus,
+    load_corpus,
+    verify_corpus_gates,
+)
+
+__all__ = [
+    "BoundaryMetrics",
+    "CorpusReport",
+    "evaluate_corpus",
+    "load_corpus",
+    "verify_corpus_gates",
+]
