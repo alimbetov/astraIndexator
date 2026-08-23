2f3d1f5fb737cf2a0c3ababc7f9eb17e111c1751
alimbetov <51784151+alimbetov@users.noreply.github.com>
2026-08-23 12:13:22 +0500

    feat: register M4.1 and M4.2 format handlers

--- src/astra_indexator/parser/__init__.py
+++ src/astra_indexator/parser/__init__.py
@@ -6,6 +6,15 @@ from .base import (
     ParserError,
     QualityProfile,
 )
+from .extended_handlers import (
+    CsvDocumentHandler,
+    EpubDocumentHandler,
+    HtmlDocumentHandler,
+    OdtDocumentHandler,
+    PptxDocumentHandler,
+    RtfDocumentHandler,
+    XlsxDocumentHandler,
+)
 from .handlers import (
     DocxDocumentHandler,
     ImageDocumentHandler,
@@ -34,11 +43,20 @@ def default_registry() -> FileTypeHandlerRegistry:
         ImageDocumentHandler("JPEG"),
         ImageDocumentHandler("PNG"),
         ImageDocumentHandler("TIFF"),
+        XlsxDocumentHandler(),
+        PptxDocumentHandler(),
+        CsvDocumentHandler(),
+        HtmlDocumentHandler(),
+        OdtDocumentHandler(),
+        RtfDocumentHandler(),
+        EpubDocumentHandler(),
     ])
 
 
 __all__ = [
     "DocumentParserService", "FileTypeHandlerRegistry", "ParseContext", "ParseLimits", "ParserError", "QualityProfile",
     "DocumentElement", "ElementType", "OcrCandidate", "ParsedDocument", "ParseQuality", "ParserIdentity", "QualityStatus", "SourceGeometry",
-    "TextDocumentHandler", "MarkdownDocumentHandler", "DocxDocumentHandler", "PdfDocumentHandler", "ImageDocumentHandler", "default_registry",
+    "TextDocumentHandler", "MarkdownDocumentHandler", "DocxDocumentHandler", "PdfDocumentHandler", "ImageDocumentHandler",
+    "XlsxDocumentHandler", "PptxDocumentHandler", "CsvDocumentHandler", "HtmlDocumentHandler", "OdtDocumentHandler", "RtfDocumentHandler", "EpubDocumentHandler",
+    "default_registry",
 ]
