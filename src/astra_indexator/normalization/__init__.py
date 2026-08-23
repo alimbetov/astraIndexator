ddc4f434535a07245cc743ed374b27bee1958834
alimbetov <51784151+alimbetov@users.noreply.github.com>
2026-08-23 19:39:17 +0500

    feat(m6): export normalization API

---
+++ src/astra_indexator/normalization/__init__.py
@@ -0,0 +1,10 @@
+from .model import NormalizationProfile, NormalizationStats, NormalizedDocument, NormalizedElement
+from .service import TextNormalizationService
+
+__all__ = [
+    "NormalizationProfile",
+    "NormalizationStats",
+    "NormalizedDocument",
+    "NormalizedElement",
+    "TextNormalizationService",
+]
