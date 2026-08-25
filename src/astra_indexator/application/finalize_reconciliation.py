fa701a841ef359573926a98e21b0e83cc7cc814a
alimbetov <51784151+alimbetov@users.noreply.github.com>
2026-08-25 23:42:59 +0500

    Use AstraVector port transport error for finalize reconciliation

--- src/astra_indexator/application/finalize_reconciliation.py
+++ src/astra_indexator/application/finalize_reconciliation.py
@@ -10,6 +10,7 @@ from sqlalchemy.orm import Session
 
 from astra_indexator.astravector.contracts import (
     AstraVectorIngestionPort,
+    AstraVectorTransportError,
     DocumentVectorStatus,
     FinalizeIngestionCommand,
     FinalizeIngestionResult,
@@ -121,9 +122,8 @@ class FinalizeReconciliationRunner:
                 finalize_attempts += 1
                 try:
                     result = self._port.finalize(command)
-                except RuntimeError as exc:
-                    code = getattr(exc, "code", None)
-                    if not isinstance(code, str) or code not in _AMBIGUOUS_FINALIZE_CODES:
+                except AstraVectorTransportError as exc:
+                    if exc.code not in _AMBIGUOUS_FINALIZE_CODES:
                         raise
                     last_status = self._observe_status(job_id, ingestion_session_id)
                     status_polls += 1
